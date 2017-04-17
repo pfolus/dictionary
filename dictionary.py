6bf1424b3bce2b85482a804fb20c28fd4cae8b30
pfolus <imjanczalover18@gmail.com>
2017-04-17 10:53:41 +0200

    Add files via upload

--- dictionary.py
+++ dictionary.py
@@ -1,20 +1,23 @@
+import csv
+
 dictio = open('dictionary.csv')
 dictionary = dictio.readlines()
 dictio.close()
 i = 1
 mydict = {}
 for i in range(len(dictionary)):
-    dictionary[i] = dictionary[i].split(';', 1) #split string into 2 element (string and double string)
-    dictionary[i][1] = tuple(dictionary[i][1].split(';')) #split double string into tuple
-    mydict[dictionary[i][0]] = dictionary[i][1] #adding items to dictionary
+    # split string into 2 element (string and double string)
+    dictionary[i] = dictionary[i].split(';', 1)
+    # split double string into tuple
+    dictionary[i][1] = tuple(dictionary[i][1].split(';'))
+    mydict[dictionary[i][0]] = dictionary[i][1]  # adding items to dictionary
     i += 1
 repeatmain = 1
-while repeatmain == 1: 
+while repeatmain == 1:
     a = input("""Dictionary for a little programmer:
             1) search explanation by appellation *
             2) add new definition
             3) show all appellations alphabetically
-            4) show available definitions by first letter of appellation **
             0) exit\n""")
     if a == '1':
         repeat1 = 1
@@ -27,36 +30,52 @@ while repeatmain == 1:
                 source = mydict.get(word)[1]
                 print(expl, 'source:', source)
             else:
-                print("\ndictionary doesn't include that word, try with another word:\n ")
+                print(
+                    "\ndictionary doesn't include that word, try with another word:\n ")
     elif a == '2':
         newapp = input('Type an appellation:')
         newdef = input('Create a definition for the new appellation:')
         newsource = input('Provide a source of that definition:')
-        newtuple = (newdef, newsource) 
-        mydict[newapp] = newtuple #adding new key&value to mydict
+        newtuple = (newdef, newsource)
+        mydict[newapp] = newtuple  # adding new key&value to mydict
         lista = []
-        for line in mydict.items(): #turning dictionary into list of nested tuples
-            for element in line: #failed to turn list of nested tuples into csv file :(
-                lista.append(line)
-        print(lista)
+        lista1 = []
+        for line in mydict.items():  # turning dictionary into list of nested tuples,
+            el1 = line[0]
+            el2 = line[1]
+            lista.append(el1)
+            lista.append(el2)
+        for element in lista:  # unnesting tuples into list of strings
+            if isinstance(element, tuple):
+                elx1 = element[0]
+                elx2 = element[1]
+                lista1.append(elx1)
+                lista1.append(elx2)
+            else:
+                lista1.append(element)
+        string = ''
+        iterator = 0
+        for element in lista1:  # turning list of strings into string
+            string += element.rstrip()
+            string += ';'
+            iterator += 1
+            if iterator % 3 == 0:
+                string += '\n'
+        print(string)
+        newdict = open('dictionary.csv', 'w')  # writing highscore to the file
+        newdict.write(string)
+        newdict.close()
+
     elif a == '3':
+        print()
         sortedkeys = sorted(mydict)
-        i = 1
+        i = 0
         for i in range(len(sortedkeys)):
             print(sortedkeys[i])
             i += 1
+        print()
     elif a == '0':
         print("Have a nice day!")
         quit()
     else:
         print("Wrong option chosen, try again!!!!")
-
-
-
-
-
-
-
-
-
-
